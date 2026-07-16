7aaa1b6ba6964191649fb6a169e23a5e9323313e
HuyHenry008 <giahuyphamthanh8@gmail.com>
2026-07-16 10:29:32 +0700

    bo sung bai 8

--- learninggo/day3/day3.go
+++ learninggo/day3/day3.go
@@ -1,1 +1,37 @@
 package main
+
+import "fmt"
+
+const MONAN = "Chao"
+
+func main() {
+
+	// const MONAN = "Com tam"
+	// fmt.Println(MONAN)
+
+	// fmt.Print("Hello Gia Huy")
+	// fmt.Print("Gia Huy")
+
+	// fmt.Println("Hello Gia Huy")
+	// fmt.Println("Gia Huy")
+
+	// var fullName = "Pham Thanh Gia Huy"
+	// var age = 22
+	// fmt.Printf("Hello %s and your age is %d. \n", fullName, age)
+
+	// var firstName, lastName string
+	// fmt.Print("Please enter your name: ")
+	// fmt.Scan(&firstName, &lastName)
+	// fmt.Println("My name is", firstName, lastName)
+
+	// var firstName, lastName string
+	// fmt.Print("Please enter your first name: ")
+	// fmt.Scanln(&firstName)
+	// fmt.Print("Please enter your last name: ")
+	// fmt.Scanln(&lastName)
+	// fmt.Println("My name is", firstName, lastName)
+
+	message := fmt.Sprint("My name is", "Huy")
+	fmt.Println(message)
+
+}
--- learninggo/day3/day3.md
+++ learninggo/day3/day3.md
@@ -1,4 +1,4 @@
-# Video 21-30
+# Video 8-10
 
 1. Xem video, thực hành theo và lưu vào day3.go 
 2. Ghi chú lại tất cả những kiến thức gì trong file day1.md
@@ -6,9 +6,16 @@
 4. Viết các lỗi thường gặp, nguyên nhân, cách giải quyết lỗi
 5. Câu hỏi chưa hiểu cần hỏi để làm rõ 
 
-## Video 21
+## Video 8
 - todo 
+- todo 
+Tìm hiểu Package fmt
+Package fmt trong Go được xây dựng để nhằm mục đích tương tác trực tiếp với dữ liệu như hiển thị dữ liệu, lấy dữ liệu từ bàn phím. Một số hàm trong package fmt hay sử dụng:
+1. Print, Println, Printf: Hiển thị dữ liệu trực tiếp ra màn hình 
+2. Scan, Scanln, Scanf: Nhập dữ liệu từ bàn phím, yêu cầu con trỏ để gán giá trị 
+3. Sprint, Sprintln, Sprint: Xử lý chuỗi và trả về kết quả (không in ra màn hình, thường dùng để lưu trữ và xử lý tiếp)
 
+CHẠY APP go run .
 ## Video 22
 - todo 
 
