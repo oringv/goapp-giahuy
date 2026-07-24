9ee592a1b3ca77967f5058496e51b256cdf3f646
GiaHuy2902 <ecomilk8@gmail.com>
2026-07-24 11:04:14 +0700

    Golang bai 40 den bai 43

--- learninggo/day12/day12.go
+++ learninggo/day12/day12.go
@@ -1,5 +1,115 @@
 package main
 
+// import (
+// 	"cmp"
+// 	"fmt"
+
+// 	// Lưu ý: Thư viện này thường phải chạy lệnh 'go get' mới có
+// 	// Nếu không muốn cài thêm, bạn có thể comment dòng này lại
+// 	"golang.org/x/exp/constraints"
+// )
+
+// // Khai báo Struct với Generics
+// type Box[T any] struct {
+// 	Content     T
+// 	Description T
+// }
+
+// // 1. Hàm in bất kỳ kiểu gì
+// func PrintValue[T any](v T) {
+// 	fmt.Println(v)
+// }
+
+// // 2. So sánh bằng
+// func IsEqual[T comparable](a, b T) bool {
+// 	return a == b
+// }
+
+// func NotIsEqual[T comparable](a, b T) bool {
+// 	return a != b
+// }
+
+// // 3. Định nghĩa Interface cho số
+// type Number interface {
+// 	constraints.Integer | constraints.Float
+// }
+
+// func Sum[T Number](a, b T) T {
+// 	return a + b
+// }
+
+// // 4. Hàm tìm Max dùng thư viện cmp chuẩn của Go 1.21+
+// func Max[T cmp.Ordered](a, b T) T {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// // 5. Hàm so sánh độ dài chuỗi
+// func MaxLengthString(a, b string) string {
+// 	if len(a) > len(b) {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func main() {
+// 	// fmt.Println("--- Kết quả chạy bài 40 ---")
+
+// 	// fmt.Print("Số lớn nhất giữa 10 và 7 là: ")
+// 	// PrintValue(Max(10, 7))
+
+// 	// fmt.Print("Số lớn nhất giữa 5.5 và 9 là: ")
+// 	// PrintValue(Max(5.5, 9))
+
+// 	// fmt.Print("Chuỗi lớn hơn (theo bảng chữ cái): ")
+// 	// PrintValue(Max("Henry", "Henry Pham"))
+
+// 	// fmt.Print("Tên có độ dài lớn hơn là: ")
+// 	// PrintValue(MaxLengthString("Tung", "Vy"))
+
+// 	// fmt.Println("\n--- Test Box với Generics ---")
+
+// 	// stringBox := Box[string]{Content: "Hoc Golang Generic", Description: "Mo ta String Box"}
+// 	// intBox := Box[int]{Content: 99, Description: 100}
+
+// 	// fmt.Printf("String Box: %v - %v\n", stringBox.Content, stringBox.Description)
+// 	// fmt.Printf("Int Box: %v - %v\n", intBox.Content, intBox.Description)
+
+// 	PrintValue(Sum(5.5, 10))
+// 	PrintValue(Sum(9, 10))
+// 	PrintValue(Sum(3.6, 6.5))
+// }
+
+//** Bài 43 **//
+
+import "fmt"
+
 func main() {
 
+	// var number int
+	// fmt.Println(number)
+
+	// var numbers [5]int
+	// fmt.Println(numbers)
+
+	// var character string
+	// fmt.Println(character)
+
+	// var character [3]string
+	// fmt.Println(characters)
+
+	var number [5]int
+	number[2] = 10 // 0 0 10 0 0
+	number[4] = 5  // 0 0 10 0 5
+	fmt.Println(number)
+
+	var numbers03 = [...]int{5, 8, 9, 10, 11}
+
+	// 2. In kiểu dữ liệu (%T) để xem Go đếm đúng không
+	fmt.Printf("Total array: %T \n", numbers03)
+
+	// 3. In toàn bộ mảng
+	fmt.Println(numbers03)
 }
--- learninggo/day12/day12.md
+++ learninggo/day12/day12.md
@@ -8,3 +8,31 @@
 
 ## Video 40
 - todo 
+Cách Dùng Generics với Comparable & CmpOrdered
+- Comparable chỉ có thể sử dụng với các kiểu dữ liệu có toán tử == và !=
+- Các kiểu dữ liệu có thể sử dụng comparabale như string, int, float, bool, struct( nếu KHÔNG chứa slice, map, function)
+- Các kiểu như slice, map, function, KHÔNG thể dùng với comparable vì không hỗ trợ phép so sánh ==.
+
+## Video 41
+- todo 
+Cách Xây Dựng Struct Với Generics – Viết Code Linh Hoạt
+Comparable là gì và khi nào nên sử dụng
+Các kiểu dữ liệu hỗ trợ comparable
+Xây dựng hàm so sánh bằng và khác nhau với generic
+Ordered trong Golang và cách sử dụng
+
+## Video 42
+- todo 
+Tự Định Nghĩa Constraint Trong Generics
+So sánh chuỗi với Comparable & CMP
+Hiểu cách Golang so sánh chuỗi dựa trên bảng ASCII
+So sánh độ dài chuỗi với len()
+Tạo và sử dụng struct với Generic 
+
+## Video 43
+- todo 
+ Làm Việc Với Array – Cách Quản Lý Dữ Liệu Hiệu Quả
+ Array trong Golang là một tập hợp các giá trị( phần tử ) có cùng kiểu dữ liệu, với kích thước cố định.
+ Khi một Array được khai báo, kích thước của nó không thể thay đổi
+
+ 
