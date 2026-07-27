22df602a80cd4c9e87326bca5aaba2a1e5873ca2
GiaHuy2902 <ecomilk8@gmail.com>
2026-07-27 18:59:14 +0700

    Golang bai 58

---
+++ learninggo/day14/day14.go
@@ -0,0 +1,36 @@
+package main // Dòng 1
+
+import (
+	"fmt"
+	"goapp-giahuy/learninggo/day13/student"
+	"goapp-giahuy/learninggo/day13/teacher"
+	"goapp-giahuy/learninggo/day13/utils"
+	"os"
+)
+
+// BẮT BUỘC PHẢI LÀ TÊN NÀY, KHÔNG ĐƯỢC SAI MỘT CHỮ
+func main() {
+	for {
+		utils.ClearScreen()
+		fmt.Println("======== 📚 HỆ THỐNG QUẢN LÝ ========")
+		fmt.Println("1. Quản lý Sinh viên")
+		fmt.Println("2. Quản lý Giảng viên")
+		fmt.Println("3. Thoát chương trình")
+		fmt.Println("====================================")
+
+		choice := utils.GetPositiveInt("👉 Chọn chức năng: ")
+
+		switch choice {
+		case 1:
+			student.SubMenu()
+		case 2:
+			teacher.MenuQuanLyGiangVien()
+		case 3:
+			fmt.Println("👋 Tạm biệt! Tác giả: Gia Huy")
+			os.Exit(0)
+		default:
+			fmt.Println("⚠️ Lựa chọn không hợp lệ!")
+			utils.ReadInput("\nNhấn Enter để chọn lại...")
+		}
+	}
+}
